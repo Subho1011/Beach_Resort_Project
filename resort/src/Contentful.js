a32c6c751d6f8f91a827b266fd0084ec709e75ff
Subhabrata Ghoshal <ghoshalsubhabrata683@gmail.com>
2024-03-29 11:41:42 +0530

    Env variable changed

--- resort/src/Contentful.js
+++ resort/src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  space: "fj9smj1uotln",
+  accessToken: "XLVFB5CVszghsTu7XRqR9jL4l2Q256kbsC_7FoW9oMw"
 });
